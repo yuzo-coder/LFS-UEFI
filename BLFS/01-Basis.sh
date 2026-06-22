330a2a2544de6ded559ce43d387fe1a397a0857e
yuzo <shiroyamayuzo@gmail.com>
2026-06-22 19:03:50 +0900

    Update 01-Basis.sh

--- BLFS/01-Basis.sh
+++ BLFS/01-Basis.sh
@@ -37,7 +37,7 @@ if swapon --show | grep -q /swapfile; then
     swapoff "/swapfile"
 fi
 
-dd if=/dev/zero of=/swapfile bs=1M count=12288
+dd if=/dev/zero of=/swapfile bs=1M count=1024
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
